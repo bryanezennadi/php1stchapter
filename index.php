478ed1c577c918b5f59c3980359bfb294422782f
bryan ezennadi <121224272+bryanezennadi@users.noreply.github.com>
2024-11-21 12:24:30 +0100

    prima lezione php 21/11/2024
    costanti, variabili, implementazione html e massimo/minimo intero. php 8.0

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+echo "ciao mondo";           // no funzione ma costrutto
+//istruzioni e dati
+// possono essere integer, float/double/realnumber boolean, string
+$count=10;
+$list_price = 14.3;
+$first_name = 'BOB';
+$first_name2 = "Alice";
+$is_valid = true;
+$microwave = 3.9e-12;
+echo '<br>';
+echo PHP_INT_MAX;   //massimo numero intero     //segno mantissa esponente
+echo '<br>';
+echo PHP_INT_MIN; //minimo intero
+//costanti
+const PIGRECO= 3.141516
+?>
+<!--<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+</head>
+<body>
+<h1> <//?php echo $var1?></h1> il browser non conosce php, il codice php viene fatto dal server
+
+</body>
+</html>-->
